ec0ed42cbf292979fd618ce0ef7b00f17537aa46
Isaac <mayne.isaac@gmail.com>
2025-02-11 00:01:26 +0100

    add create product

--- src/router.ts
+++ src/router.ts
@@ -1,13 +1,14 @@
 import { Router } from "express";
+import { createProduct } from "./handlers/product";
 const router = Router();
 
 //routing
 router.get("/", (req, res) => { 
     res.json('desde GET');
 })
-router.post("/", (req, res) => { 
-    res.json('desde POST');
-})
+
+router.post("/", createProduct);
+
 router.put("/", (req, res) => { 
     res.json('desde put');
 })
